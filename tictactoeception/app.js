ac1636c13dd1f0bfae50fc493997d7041bc509a7
CameronC17 <cameronchalmers@hotmail.co.uk>
2016-09-13 16:44:27 +0100

    completed master tic tac toe inception

--- tictactoeception/app.js
+++ tictactoeception/app.js
@@ -11,6 +11,11 @@ window.onload = function() {
 
 class TTTMulti {
   constructor() {
+  	this.table = document.getElementById("scoreboard");
+    this.board = [];
+    this.winner = [null, null, null, null, null, null, null, null, null];
+    this.globalWinner = null;
+    this.turnCounter = 0;
     this.createGameBoard();
   }
 
@@ -25,115 +30,121 @@ class TTTMulti {
 		  	var button = document.createElement("button");
 
 		  	button.addEventListener("click", function(event) {
-		  		window.alert("blabla");
-
+		  		this.clickedOn(event.target);
 	      	}.bind(this));
 
 		  	//button.id = buttonName;
 		  	button.setAttribute("id", buttonName);
+		  	button.style.cssText = 'width:40px;height:40px;display:inline-block;float:left;';
 			div.appendChild(button);
 		}
+		div.style.cssText = 'display:inline-block;margin:17px;width:120px;height: 120px;float: left;';
 		var element = document.getElementById("game");
 		element.appendChild(div);
+		this.board.push([null, null, null, null, null, null, null, null, null]);
 	}
   }
 
+  clickedOn(button) {
+  	this.checkMarker(button);
+  	this.checkWin();
+  	this.updateScoreboard();
+  	this.checkGlobalWin();
+  }
 
+  checkMarker(button) {
+  	if (this.board[button.id.charAt(0) - 1][button.id.charAt(1) - 1] == null && this.winner[button.id.charAt(0) - 1] == null) {
+  		if (this.turnCounter % 2 == 0) {
+  			button.textContent = "X";
+  			this.board[button.id.charAt(0) - 1][button.id.charAt(1) - 1] = "X";
+  			this.turnCounter++;
+  		} else {
+  			button.textContent = "O";
+  			this.board[button.id.charAt(0) - 1][button.id.charAt(1) - 1] = "O";
+  			this.turnCounter++;
+  		}
+  	}
+  }
 
+  checkWin() {
+  	//for each table
+  	for (var x = 0; x < 9; x++) {
 
-}
-
-
-
-/*var turnCounter = 0;
-var tableArray = [null, null, null, null, null, null, null, null, null];
-var winner = null;
-var scoreboard = [0,0];
-
-window.onload = function() {
-	var buttons = Array.from(document.getElementsByTagName("button"));
-	var table = document.getElementById("scoreboard");
-
-	buttons.forEach(function(button) {
-    button.addEventListener("click", function() {
-    	if (this.id == "clear") {
-    		resetBoard();
-    	} else if (tableArray[this.id-1] == null) {
-    		if (turnCounter % 2 == 0) {
-    			this.textContent = "X";
-    			tableArray[this.id - 1] = "X";
-    			turnCounter++;
-    		} else {
-    			this.textContent = "O";
-    			tableArray[this.id - 1] = "O";
-    			turnCounter++;
-    		}
-
-    		checkWin();
-    	}
-    	});
-	});
-
-	function checkWin() {
-		//check horizontal
+  		//check horizontal
 		for (var i = 0; i < 8; i+=3) {
-			if (tableArray[i] == "X" && tableArray[i + 1] == "X" && tableArray[i + 2] == "X") {
-				winner = "X";
+			if (this.board[x][i] == "X" && this.board[x][i + 1] == "X" && this.board[x][i + 2] == "X") {
+				this.winner[x] = "X";
 			}
-			else if (tableArray[i] == "O" && tableArray[i + 1] == "O" && tableArray[i + 2] == "O") {
-				winner = "O";
+			else if (this.board[x][i] == "O" && this.board[x][i + 1] == "O" && this.board[x][i + 2] == "O") {
+				this.winner[x] = "O";
 
 			}
 		}
 		//check vertical
 		for (var i = 0; i < 3; i++) {
-			if (tableArray[i] == "X" && tableArray[i + 3] == "X" && tableArray[i + 6] == "X") {
-				winner = "X";
+			if (this.board[x][i] == "X" && this.board[x][i + 3] == "X" && this.board[x][i + 6] == "X") {
+				this.winner[x] = "X";
 			}
-			else if (tableArray[i] == "O" && tableArray[i + 3] == "O" && tableArray[i + 6] == "O") {
-				winner = "O";
+			else if (this.board[x][i] == "O" && this.board[x][i + 3] == "O" && this.board[x][i + 6] == "O") {
+				this.winner[x] = "O";
 
 			}
 		}
-		//check horizontal
-		if ((tableArray[0] == "X" && tableArray[4] == "X" && tableArray[8] == "X") || (tableArray[2] == "X" && tableArray[4] == "X" && tableArray[6] == "X")) {
-			winner = "X";
+		//check diagonal
+		if ((this.board[x][0] == "X" && this.board[x][4] == "X" && this.board[x][8] == "X") || (this.board[x][2] == "X" && this.board[x][4] == "X" && this.board[x][6] == "X")) {
+			this.winner[x] = "X";
 		}
-		if ((tableArray[0] == "O" && tableArray[4] == "O" && tableArray[8] == "O") || (tableArray[2] == "O" && tableArray[4] == "O" && tableArray[6] == "O")) {
-			winner = "O";
+		if ((this.board[x][0] == "O" && this.board[x][4] == "O" && this.board[x][8] == "O") || (this.board[x][2] == "O" && this.board[x][4] == "O" && this.board[x][6] == "O")) {
+			this.winner[x] = "O";
+		}
+  	}
+
+  }
+
+  updateScoreboard() {
+  	var scoreboard = [0,0];
+  	for (var i = 0; i < 9; i++) {
+  		if (this.winner[i] == "X")
+  			scoreboard[0]++;
+  		else if (this.winner[i] == "O")
+  			scoreboard[1]++;
+  	}
+  	this.table.rows[1].cells[0].innerHTML = scoreboard[0];
+	this.table.rows[1].cells[1].innerHTML = scoreboard[1];
+  }
+
+  checkGlobalWin() {
+  	//check horizontal
+		for (var i = 0; i < 8; i+=3) {
+			if (this.winner[i] == "X" && this.winner[i + 1] == "X" && this.winner[i + 2] == "X") {
+				this.globalWinner = "X";
+			}
+			else if (this.winner[i] == "O" && this.winner[i + 1] == "O" && this.winner[i + 2] == "O") {
+				this.globalWinner = "O";
+
+			}
+		}
+		//check vertical
+		for (var i = 0; i < 3; i++) {
+			if (this.winner[i] == "X" && this.winner[i + 3] == "X" && this.winner[i + 6] == "X") {
+				this.globalWinner = "X";
+			}
+			else if (this.winner[i] == "O" && this.winner[i + 3] == "O" && this.winner[i + 6] == "O") {
+				this.globalWinner = "O";
+
+			}
+		}
+		//check vertical
+		if ((this.winner[0] == "X" && this.winner[4] == "X" && this.winner[8] == "X") || (this.winner[2] == "X" && this.winner[4] == "X" && this.winner[6] == "X")) {
+			this.globalWinner = "X";
+		}
+		if ((this.winner[0] == "O" && this.winner[4] == "O" && this.winner[8] == "O") || (this.winner[2] == "O" && this.winner[4] == "O" && this.winner[6] == "O")) {
+			this.globalWinner = "O";
 		}
 
-		//amend the scoreboard
-		if (winner != null) {
-			window.alert("The winner is: " + winner);
-			if (winner == "X")
-				scoreboard[0]++;
-			else if (winner == "O")
-				scoreboard[1]++;
-
-			//console.log(table.rows[1].cells[0]);
-			table.rows[1].cells[0].innerHTML = scoreboard[0];
-			table.rows[1].cells[1].innerHTML = scoreboard[1];
+		if (this.globalWinner != null) {
+			window.alert("THE GLOBAL WINNER IS: " + this.globalWinner);
 		}
+  }
 
-		//check draw
-		var drawCounter = 0;
-		for (var i = 0; i < tableArray.length; i++) {
-			if (tableArray[i] != null)
-				drawCounter++;
-		}
-
-		if (drawCounter == 9 && winner == null) {
-			window.alert("Nobody has won. HOW?!?!");
-		}
-	}
-
-	function resetBoard() {
-		tableArray = [null, null, null, null, null, null, null, null, null];
-		turnCounter = 0;
-		winner = null;
-		for (var i = 0; i < buttons.length - 1; i++) {
-			buttons[i].textContent = "";
-		}
-	}
-}*/
+}
